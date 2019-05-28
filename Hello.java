bc138f40347bca23887f75bf061f0ad99c4a6f73
ramasai-badam <35661607+ramasai-badam@users.noreply.github.com>
2019-05-28 16:02:37 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,12 @@
-public void Hello(){
-  public static void main(String[] args) {
-    System.out.println("hello world !!");
-  }
+public class Hello {
+
+    public static void main(String[] args) {
+        
+        int first = 10;
+        int second = 20;
+
+        int sum = first + second;
+
+        System.out.println("The sum is: " + sum);
+    }
 }
